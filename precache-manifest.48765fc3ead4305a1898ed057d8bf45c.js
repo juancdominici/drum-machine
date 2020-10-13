37df117ec2cc379427923b549ab57e4f55f988f7
Reshi <juancdominici@gmail.com>
2020-10-13 16:13:53 -0300

    Updates

---
+++ precache-manifest.48765fc3ead4305a1898ed057d8bf45c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d78b9ce05452804b4d3e200516d6bb76",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "28fad73e6b81075e023f",
+    "url": "/drum-machine/static/css/main.233af343.chunk.css"
+  },
+  {
+    "revision": "e624798c9f83e6b2d4c2",
+    "url": "/drum-machine/static/js/2.537d1605.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.537d1605.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28fad73e6b81075e023f",
+    "url": "/drum-machine/static/js/main.65282a89.chunk.js"
+  },
+  {
+    "revision": "d7d1943964fcc80e9287",
+    "url": "/drum-machine/static/js/runtime-main.49a0ba3c.js"
+  }
+]);
